6a60c8242ee5d3de6a782ca4ce43f4071fec475c
Soulmatee <danybad200705@gmail.com>
2026-02-01 22:33:45 +0200

    Add files via upload

--- mixin/ServerLevelMixin.java
+++ mixin/ServerLevelMixin.java
@@ -3,13 +3,18 @@ package net.soulmate.rpg_soul.mixin;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.soulmate.rpg_soul.util.MultiNoiseBiomeSourceAccessor;
+import net.soulmate.rpg_soul.worldgen.biome.ModBiomeRarity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerLevel.class)
-public class ServerLevelMixin {
+public abstract class ServerLevelMixin {
+    @Shadow
+    public abstract long getSeed();
+
     @Inject(method = "<init>", at = @At("TAIL"))
     private void rpg_soul$passDimensionToBiomeSource(CallbackInfo ci) {
         ServerLevel level = (ServerLevel) (Object) this;
@@ -18,5 +23,8 @@ public class ServerLevelMixin {
             accessor.setLastSampledDimension(level.dimension());
             accessor.setLastSampledSeed(level.getSeed());
         }
+    }
+    private void rpg_soul$initVoronoi(CallbackInfo ci) {
+        ModBiomeRarity.init(this.getSeed());
     }
 }
