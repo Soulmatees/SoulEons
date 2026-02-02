848fb1cbd7a4d90584e2896ead742bb7bf290c77
Soulmatee <danybad200705@gmail.com>
2026-02-02 22:18:21 +0200

    Delete util directory

--- util/MultiNoiseBiomeSourceAccessor.java
+++
@@ -1,9 +0,0 @@
-package net.soulmate.rpg_soul.util;
-
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.level.Level;
-
-public interface MultiNoiseBiomeSourceAccessor {
-    void setLastSampledSeed(long seed);
-    void setLastSampledDimension(ResourceKey<Level> dimension);
-}
