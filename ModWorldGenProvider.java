469c3d2b6a14df4504a1ebba2323bc2621e311fc
Soulmatee <danybad200705@gmail.com>
2026-02-02 22:05:07 +0200

    Add files via upload

--- ModWorldGenProvider.java
+++ ModWorldGenProvider.java
@@ -4,30 +4,26 @@ import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
-import net.minecraftforge.registries.ForgeRegistries;
 import net.soulmate.rpg_soul.RPG_Soul;
-import net.soulmate.rpg_soul.worldgen.*;
+import net.soulmate.rpg_soul.worldgen.ModDensityFunctions;
+import net.soulmate.rpg_soul.worldgen.ModNoiseSettings;
 import net.soulmate.rpg_soul.worldgen.biome.ModBiomes;
+import net.soulmate.rpg_soul.worldgen.noise.ModNoises;
 
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 public class ModWorldGenProvider extends DatapackBuiltinEntriesProvider {
-
     public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
             .add(Registries.NOISE, ModNoises::bootstrap)
-            .add(Registries.BIOME, ModBiomes::bootstrap)
             .add(Registries.DENSITY_FUNCTION, ModDensityFunctions::bootstrap)
-            .add(Registries.NOISE_SETTINGS, ModNoiseSettings::bootstrap)
-            .add(Registries.CONFIGURED_CARVER, ModCarvers::bootstrap)
-            .add(Registries.WORLD_PRESET, ModWorldPreset::bootstrap);
-
-
+            .add(Registries.BIOME, ModBiomes::bootstrap)
+            .add(Registries.NOISE_SETTINGS, ModNoiseSettings::bootstrap);
 
     public ModWorldGenProvider(
             PackOutput output,
             CompletableFuture<net.minecraft.core.HolderLookup.Provider> registries
     ) {
-        super(output, registries, BUILDER, Set.of(RPG_Soul.MOD_ID));
+        super(output, registries, BUILDER, Set.of(RPG_Soul.MOD_ID, "minecraft"));
     }
 }
